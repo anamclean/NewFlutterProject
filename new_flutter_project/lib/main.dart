1a271002f10762b67f951d301510f1ba6657798a
anamclean <anamclean13@gmail.com>
2023-01-30 12:57:33 -0600

    style, foreground color, background color

--- new_flutter_project/lib/main.dart
+++ new_flutter_project/lib/main.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Center(child:Text("My First Project")),
-
+      home: Scaffold(
+    appBar: AppBar(
+      title: Center(child: Text("My First Project")),
     ),
-      body:  Center(child:Text("Learning",
-        style: TextStyle(fontSize: 24),)
-      ),
-    )
-  ));
+    body: Center(
+        child: Text(
+      "Learning",
+      style: TextStyle(fontSize: 24),
+    )),
+    bottomNavigationBar: TextButton(
+        child: Text("Click me"),
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.amber, backgroundColor: Colors.cyanAccent,
+        ),
+        onPressed: () {}),
+  )));
 }
-
