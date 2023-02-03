7a15aa099e0fe37773552140195c89b738c3d638
anamclean <anamclean13@gmail.com>
2023-02-03 12:38:42 -0600

    image, colors foreground

--- new_flutter_project/lib/main.dart
+++ new_flutter_project/lib/main.dart
@@ -4,17 +4,17 @@ void main() {
   runApp(MaterialApp(
       home: Scaffold(
     appBar: AppBar(
+      backgroundColor: Colors.blue,
       title: Center(child: Text("My First Project")),
     ),
     body: Center(
-        child: Text(
-      "Learning",
-      style: TextStyle(fontSize: 24),
-    )),
+        child: Image(image: NetworkImage("https://neopraxis.mx/wp-content/uploads/2022/06/Que-es-la-psicologia.jpg") ,
+        )
+    ),
     bottomNavigationBar: TextButton(
         child: Text("Click me"),
         style: TextButton.styleFrom(
-          foregroundColor: Colors.amber, backgroundColor: Colors.cyanAccent,
+          foregroundColor: Colors.amber, backgroundColor: Colors.blue,
         ),
         onPressed: () {}),
   )));
